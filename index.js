0bb6f63ddc6acb8177efecced7f0f156b0dd1192
Nicholas Westerhausen <nicholas@westerhausen.me>
2020-09-17 19:06:02 -0400

    Moved all my code to 'lib' and using index to just start.

--- index.js
+++ index.js
@@ -1,33 +1,7 @@
-const express = require('express')
-const app = express();
-const http = require('http').createServer(app);
-const morgan = require('morgan');
-const io = require('socket.io')(http);
+const server = require('./lib/server');
 
-const path = require('path');
-const config = require('config');
 
-const configure = require('./lib/configure');
-const constants = require('./lib/constants');
-
-const RUNNING_PORT = config.get('app.port');
-
-// Logging
-app.use(morgan('short'));
-// Static Serving
-app.use(express.static(path.join(__dirname, "web")));
-
-io.on('connection', (socket) => {
-    console.log(`${socket.handshake.address} - WS client ${socket.id} | connection`);
-    socket.on(constants.SCK_CONFIG_SET_FILENAME, (filename) => {
-        configure.setConfigFile(filename);
-    })
-    socket.on(constants.SCK_CONFIG_SET_WEBPORT, (val) => {
-        configure.setWebPort(val);
-    })
-})
-
-// Run Web Server
-http.listen(RUNNING_PORT, () => {
-    console.log(`Web interface available at http://localhost:${RUNNING_PORT}/`)
-});
+server.startServer();
+setTimeout(() => {
+    server.restartServer()
+}, 3000)
--- web/js/main.js
+++ web/js/main.js
@@ -1,1 +1,18 @@
 let socket = io();
+
+socket.on("connect",function ()  {
+    console.log("Connection established via socket.");
+});
+
+socket.on("disconnect",function ()  {
+    console.log("Connection via socket closed.");
+});
+
+socket.on('server restart', function() {
+    console.warn("Server sent server restart message, closing socket!");
+    socket.disconnect();
+    setTimeout(() => {
+        socket.connect();
+    }, 8000);
+    console.log("Attempting socket reconnection in 8s.");
+})
