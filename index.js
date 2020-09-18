2c6f9b4aaaad7d38806d812875267511427dc0e3
Nicholas Westerhausen <nwesterhausen@gmail.com>
2020-09-18 09:33:22 -0400

    planned tasks

--- index.js
+++ index.js
@@ -1,4 +1,12 @@
 const server = require('./lib/server');
 
+// TODO: rename included config files to sample.config.yaml and sample.domain.yaml
+// TODO: check if config.yaml and domain.yaml are in config folder, if not copy samples for that
+
+// TODO: set expectation that app config is in config.yaml
+// TODO: set expectation that domain config is in domain.yaml
+
+// TODO: how to handle WHOIS results and where to keep track of that.
+
 // May end up doing more in this, but for now just launches the server.
 server.startServer();
