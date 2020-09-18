8fc186b341db3bc76c059d8d5c9ab2ca5a608087
Nicholas Westerhausen <nwesterhausen@gmail.com>
2020-09-18 09:23:44 -0400

    Added domain configuration page. Using a duplicated constants file for the client. Using a constant for the 'server restart' message.

--- web/js/main.js
+++ web/js/main.js
@@ -17,12 +17,21 @@ socket.on('server restart', function() {
     console.log("Attempting socket reconnection in 8s.");
 });
 
-socket.on('101', function(data) {
-    console.log("Configuration", data);
-    document.querySelector("#webappPort").setAttribute('placeholder', data.app.port);
-    document.querySelector("#smtpHost").setAttribute('placeholder', data.smtp.host);
-    document.querySelector("#smtpPort").setAttribute('placeholder', data.smtp.port);
-    document.querySelector("#secureSmtpCheck").setAttribute('placeholder', data.smtp.secure);
-    document.querySelector("#smtpUser").setAttribute('placeholder', data.smtp.auth.user);
-    document.querySelector("#smtpPass").setAttribute('placeholder', '*****');
+socket.on(SCK_LOADED_CFG, function(data) {
+    console.log("Configuration data received", data);
+    updateConfigPlaceholders(data);
 })
+
+/**
+ * Fill in the 'placeholder' attribute on the configuration form with configuration
+ * data we get from the server (hopefully live config or something).
+ * @param configData
+ */
+function updateConfigPlaceholders(configData) {
+    document.querySelector("#webappPort").setAttribute('placeholder', configData.app.port);
+    document.querySelector("#smtpHost").setAttribute('placeholder', configData.smtp.host);
+    document.querySelector("#smtpPort").setAttribute('placeholder', configData.smtp.port);
+    document.querySelector("#secureSmtpCheck").setAttribute('placeholder', configData.smtp.secure);
+    document.querySelector("#smtpUser").setAttribute('placeholder', configData.smtp.auth.user);
+    document.querySelector("#smtpPass").setAttribute('placeholder', '********');
+}
