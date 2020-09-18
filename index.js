0e4949104dbcc2a1e009763b7cf47f6d638b0be3
Nicholas Westerhausen <nwesterhausen@gmail.com>
2020-09-18 13:39:17 -0400

    Create issues from todo comments
    Created issue #1 and #2 from todos

--- index.js
+++ index.js
@@ -1,12 +1,4 @@
 const server = require('./lib/server');
 
-// TODO: rename included config files to sample.config.yaml and sample.domain.yaml
-// TODO: check if config.yaml and domain.yaml are in config folder, if not copy samples for that
-
-// TODO: set expectation that app config is in config.yaml
-// TODO: set expectation that domain config is in domain.yaml
-
-// TODO: where to keep track of whoisdata.
-
 // May end up doing more in this, but for now just launches the server.
 server.startServer();
