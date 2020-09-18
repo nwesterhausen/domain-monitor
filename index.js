c2df48d38916ce05f455ea511abfa17843c406be
Nicholas Westerhausen <nwesterhausen@gmail.com>
2020-09-18 09:10:11 -0400

    Removed testing call to restartServer()

--- index.js
+++ index.js
@@ -1,7 +1,4 @@
 const server = require('./lib/server');
 
-
+// May end up doing more in this, but for now just launches the server.
 server.startServer();
-setTimeout(() => {
-    server.restartServer()
-}, 3000)
