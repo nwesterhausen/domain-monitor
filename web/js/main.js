422a763263a266b2964f37e96fb9c20c8f33bbb0
Nicholas Westerhausen <nicholas@westerhausen.me>
2020-09-20 20:13:21 -0400

    Added functionality to the domain configuration. You can add a domain, update a domain, or remove one. Need to establish an id for each domain to make process simpler, but for now depends on fqdn being unique. Tweaked configure.js a little bit to make saving the domain list possible. It really needs another pass to simplify its functionality.

--- web/js/main.js
+++ web/js/main.js
@@ -3,17 +3,25 @@ let socket = io();
 const EDIT_SVG = '<svg width="1em" height="1em" viewBox="0 0 16 16" class="bi bi-pencil-square" fill="currentColor" xmlns="http://www.w3.org/2000/svg">\n' +
     '  <path d="M15.502 1.94a.5.5 0 0 1 0 .706L14.459 3.69l-2-2L13.502.646a.5.5 0 0 1 .707 0l1.293 1.293zm-1.75 2.456l-2-2L4.939 9.21a.5.5 0 0 0-.121.196l-.805 2.414a.25.25 0 0 0 .316.316l2.414-.805a.5.5 0 0 0 .196-.12l6.813-6.814z"/>\n' +
     '  <path fill-rule="evenodd" d="M1 13.5A1.5 1.5 0 0 0 2.5 15h11a1.5 1.5 0 0 0 1.5-1.5v-6a.5.5 0 0 0-1 0v6a.5.5 0 0 1-.5.5h-11a.5.5 0 0 1-.5-.5v-11a.5.5 0 0 1 .5-.5H9a.5.5 0 0 0 0-1H2.5A1.5 1.5 0 0 0 1 2.5v11z"/>\n' +
-    '</svg>';
+    '</svg>',
+    ADD_SVG = '<svg width="1em" height="1em" viewBox="0 0 16 16" class="bi bi-plus-square" fill="currentColor" xmlns="http://www.w3.org/2000/svg">\n' +
+        '  <path fill-rule="evenodd" d="M14 1H2a1 1 0 0 0-1 1v12a1 1 0 0 0 1 1h12a1 1 0 0 0 1-1V2a1 1 0 0 0-1-1zM2 0a2 2 0 0 0-2 2v12a2 2 0 0 0 2 2h12a2 2 0 0 0 2-2V2a2 2 0 0 0-2-2H2z"/>\n' +
+        '  <path fill-rule="evenodd" d="M8 4a.5.5 0 0 1 .5.5v3h3a.5.5 0 0 1 0 1h-3v3a.5.5 0 0 1-1 0v-3h-3a.5.5 0 0 1 0-1h3v-3A.5.5 0 0 1 8 4z"/>\n' +
+        '</svg>';
 
+const DOMAIN_EDIT_MODAL = new bootstrap.Modal(document.querySelector('#editDomainModal'), {
+    backdrop: 'static',
+});
+
+
+// Listeners
 socket.on("connect",function ()  {
     console.log("Connection established via socket.");
 });
-
 socket.on("disconnect",function ()  {
     console.log("Connection via socket closed.");
 });
-
-socket.on('server restart', function() {
+socket.on(SCK_SERVER_RESTART, function() {
     console.warn("Server sent server restart message, closing socket!");
     socket.disconnect();
     setTimeout(() => {
@@ -21,16 +29,18 @@ socket.on('server restart', function() {
     }, 8000);
     console.log("Attempting socket reconnection in 8s.");
 });
-
 socket.on(SCK_LOADED_CFG, function(data) {
     console.log("Configuration data received", data);
     updateConfigPlaceholders(data);
-})
-
+});
 socket.on(SCK_LOADED_DOMAINS, function(data) {
     console.log("Domain configuration received", data);
     updateDomainConfigurationFeedback(data);
-})
+});
+
+document.querySelector('#modalDomainBtnCommit').addEventListener('click', updateDomainInfo);
+document.querySelector('#modalDomainBtnDelete').addEventListener('click', deleteDomainInfo);
+
 
 /**
  * Fill in the 'placeholder' attribute on the configuration form with configuration
@@ -38,14 +48,21 @@ socket.on(SCK_LOADED_DOMAINS, function(data) {
  * @param configData config object send from server.
  */
 function updateConfigPlaceholders(configData) {
-    document.querySelector("#webappPort").setAttribute('placeholder', configData.app.port);
-    document.querySelector("#smtpHost").setAttribute('placeholder', configData.smtp.host);
-    document.querySelector("#smtpPort").setAttribute('placeholder', configData.smtp.port);
-    document.querySelector("#smtpUser").setAttribute('placeholder', configData.smtp.auth.user);
-    document.querySelector("#smtpPass").setAttribute('placeholder', '********');
-    document.querySelector("#smtpTarget").setAttribute('placeholder', configData.alerts.admin);
+    document.querySelector("#webappPort").setAttribute('value', configData.app.port);
+    document.querySelector("#smtpHost").setAttribute('value', configData.smtp.host);
+    document.querySelector("#smtpPort").setAttribute('value', configData.smtp.port);
+    document.querySelector("#smtpUser").setAttribute('value', configData.smtp.auth.user);
+    document.querySelector("#smtpPass").setAttribute('value', configData.smtp.auth.pass);
+    document.querySelector("#smtpTarget").setAttribute('value', configData.alerts.admin);
     document.querySelector("#enableSmtpCheck").setAttribute('checked', configData.alerts.sendalerts);
     document.querySelector("#secureSmtpCheck").setAttribute('checked', configData.smtp.secure);
+}
+
+function handleNewDomain() {
+    document.querySelector('#modalDomainTitle').innerText = "Create Domain";
+    document.querySelector('#modalDomainBtnDelete').classList.add('d-none');
+
+    DOMAIN_EDIT_MODAL.toggle();
 }
 
 function updateDomainConfigurationFeedback(domainData) {
@@ -54,6 +71,18 @@ function updateDomainConfigurationFeedback(domainData) {
     for (let i =0; i< domainData.domains.length; i++) {
         tbody.appendChild(buildDomainEntry(domainData.domains[i], i));
     }
+    let addBtn = document.createElement('A');
+    addBtn.setAttribute('href','#')
+    addBtn.id = 'btnNewDomain';
+    addBtn.addEventListener('click', handleNewDomain);
+    addBtn.innerHTML = ADD_SVG;
+
+
+    let addRow = document.createElement('tr');
+    addRow.innerHTML = '<td></td><td></td><td></td>'
+    addRow.appendChild(addBtn);
+
+    tbody.appendChild(addRow);
 }
 
 /**
@@ -115,5 +144,35 @@ function handleEditRow(e) {
         alerts: targetCells[2].innerText === "Yes"
     }
 
+    document.querySelector('#modalDomainName').setAttribute('value', domainData.name);
+    document.querySelector('#modalDomainFqdn').setAttribute('value', domainData.fqdn);
+    document.querySelector('#modalDomainAlert').setAttribute('checked', domainData.alerts);
+    document.querySelector('#modalDomainTitle').innerText = "Edit Domain";
+    document.querySelector('#modalDomainBtnDelete').classList.remove('d-none');
+
+    DOMAIN_EDIT_MODAL.toggle();
+
     console.log(targetId, domainData);
+}
+
+function updateDomainInfo() {
+    console.log('Attempting domain update.')
+    let domainData = {
+        name: document.querySelector('#modalDomainName').value,
+        fqdn: document.querySelector('#modalDomainFqdn').value,
+        alerts: document.querySelector('#modalDomainAlert').value,
+    }
+    socket.emit(SCK_UPDATE_DOMAIN, domainData);
+    DOMAIN_EDIT_MODAL.hide();
+}
+
+function deleteDomainInfo() {
+    console.log('Attempting domain deletion');
+    let domainData = {
+        name: document.querySelector('#modalDomainName').value,
+        fqdn: document.querySelector('#modalDomainFqdn').value,
+        alerts: document.querySelector('#modalDomainAlert').value,
+    }
+    socket.emit(SCK_DELETE_DOMAIN, domainData);
+    DOMAIN_EDIT_MODAL.hide();
 }
