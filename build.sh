0d4f777a1ed7d7e40c8fd0f03d022b6d30e1ca09
Nicholas Westerhausen <nwesterhausen@gmail.com>
2020-10-01 15:08:10 -0400

    Wrote a docker image build script

--- build.sh
+++ build.sh
@@ -12,21 +12,26 @@ IMAGE_PREFIX=$(jq .repository.url package.json | cut -d/ -f 5 | cut -d. -f 1)
 # Name for this image
 IMAGE=server
 # Get version tag from package.json
-TAG=$(jq .version package.json)
+TAG=$(jq .version package.json | cut -d\" -f 2)
 
 # Concat strings
 VERSIONED_TAG="docker.pkg.github.com/$USERNAME/$IMAGE_PREFIX/$IMAGE:$TAG"
-LATEST_TAG="docker.pkg.github.com/$USERNAME/$IMAGE_PREFIX/$IMAGE:latest"
 
 # Build the docker image
-docker build -t "$VERSIONED_TAG" .
+docker build -t "$USERNAME/$IMAGE_PREFIX" .
 
-# Also build a latest
-docker build -t "$LATEST_TAG" .
+# Grab the image ID
+IMAGE_ID=$(docker images -q "$USERNAME/$IMAGE_PREFIX")
+echo $IMAGE_ID
+
+# Tag the image
+docker tag $IMAGE_ID "$VERSIONED_TAG"
 
 # Login to gh packages docker repo
 cat ~/.gh_token | docker login docker.pkg.github.com -u nwesterhausen --password-stdin
 
 # Push the images
 docker push "$VERSIONED_TAG"
-docker push "$LATEST_TAG"
+
+# You could then go on to include other docker package repositories here,
+# by tagging and pushing as appropriate.
